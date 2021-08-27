b8380229a9686c5d9d62306d12f38c2cd71487e5
Hammad Muhammad Siddique <74074430+Hammad-Muhammad-Siddique@users.noreply.github.com>
2021-08-27 16:35:53 +0500

    Add files via upload

---
+++ Unity Programming Part 1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+//________________________________________________________________________________________
+//________________________________________________________________________________________
+//                                     START METHOD        
+
+//        void start()
+//        { 
+//        }
+
+//________________________________________________________________________________________
+//                                     UPDATE METHOD
+
+//       void update()
+//       {
+//       }
+
+//________________________________________________________________________________________
+//                                     MOUSEDOWN METHOD
+
+//      private void onMouseDown()
+//      {
+//      }
+
+//________________________________________________________________________________________
+//                                     COLLISION METHOD
+
+//      private void onCollisionEnter(Collision collision)
+//      {
+//      }
+
+//________________________________________________________________________________________
+//                                    DESTROYING OBJECTS
+
+//       Destroy(gameObject);
+
+//________________________________________________________________________________________
+//                                   DETECTING MOUSE CLICKS 
+
+//       Destroy(gameObject);
+
+//________________________________________________________________________________________
+//                                   TAKING KEYBOARD INPUTS
+
+//       Input.GetKeyDown(KeyCode.key);
+
+//________________________________________________________________________________________
+//                                    ADDING PHYSICS
+
+//       rb = GetComponent<rigidbody>();
+
+//________________________________________________________________________________________
+//                                    ADDING FORCE
+
+//       rb.AddForce(Vector3.direction * amount);
+
+//________________________________________________________________________________________
+//                                    ADDING VELOCITY
+
+//       rb.velocity = Vector3.direction * amountf
+
+//________________________________________________________________________________________
+//                                    COLLISION DETECTION
+
+//      if (collision.gameObject.tag == "Enemy") 
+//      {
+//          Destroy(collision.gameObject);
+//      }
+
+
+//________________________________________________________________________________________
+//                                    LOADING NEW LEVEL
+
+//      SceneManager.LoadScene("Scene Name");
+
+//________________________________________________________________________________________
+//                                    MOVING PLAYER
+
+//      float xInput;
+//      flaot zInput;
+//      public float speed;
+//      xInput = Input.GetAxis("Horizontal");
+//      zInput = Input.GetAxis("Vertical");
+//      rb.AddForce(xInput * speed, 0, zInput * speed);
+
+//________________________________________________________________________________________
+// 
+
+    }
+}
